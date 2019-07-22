33661d920089347ee93ccd56879e5f8f50b18bd6
Chanmi Kim <chanmi_dev@naver.com>
2019-07-23 02:39:42 +0900

    Add File [SpringBegin > src > com.test.spring.di_5 > Ex03_File.java]
    1. java 파일 추가
    
    - Ex03_File.java

---
+++ SpringBegin/src/com/test/spring/di_5/Ex03_File.java
@@ -0,0 +1,45 @@
+package com.test.spring.di_5;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+public class Ex03_File implements IWorker {
+
+	public ArrayList<String> list() {
+		
+		ArrayList<String> list = new ArrayList<String>();
+		
+		try {
+			
+			BufferedReader reader = new BufferedReader(new FileReader("data.db"));
+			
+			String temp = "";
+			
+			while ((temp = reader.readLine()) != null) {
+				list.add(temp);
+			}
+			
+			reader.close();
+			
+			return list;
+						
+		} catch (Exception e) {
+			System.out.println("Ex03_File.list : " + e.toString());
+		}
+		
+		return null;
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
