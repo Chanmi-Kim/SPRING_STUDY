f2c4dac9482c2d890410f5c862e494527fc07c96
Chanmi Kim <chanmi_dev@naver.com>
2019-07-23 02:39:20 +0900

    Add File [SpringBegin > src > com.test.spring.di_5 > Ex03.java]
    1. java 파일 추가
    
    - Ex03.java

---
+++ SpringBegin/src/com/test/spring/di_5/Ex03.java
@@ -0,0 +1,62 @@
+package com.test.spring.di_5;
+
+import java.util.ArrayList;
+
+public class Ex03 {
+
+	public static void main(String[] args) {
+		
+		//[관계]
+		//Ex02 -> (위임) -> Util -> (위임) -> Scanner
+		//Ex03 -> (위임) -> Service -> (위임) -> File or DB
+		
+		//[업무 데이터]
+		//1. 파일 데이터 > (전환) > 2. DB 데이터
+		
+		//업무 > 데이터소스의 이름 명단을 가져와서 화면에 출력
+		
+		//IWorker worker = new Ex03_File();
+		IWorker worker = new Ex03_DB();
+		
+		Ex03_Service service = new Ex03_Service(worker); //의존 주입
+		
+		ArrayList<String> list = service.list();
+		
+//		View view = new View();
+//		view.print(list);
+		
+		//뷰 역할
+		for (String name : list) {
+			System.out.println(name);
+		}
+		
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
