7dd66345a04a603ae095a68e9a791f1c566f789b
Chanmi Kim <chanmi_dev@naver.com>
2019-07-23 02:42:13 +0900

    Add File [SpringBegin > src > com.test.spring.di_7 > File.java]
    1. java 파일 추가
    
    - File.java

---
+++ SpringBegin/src/com/test/spring/di_7/File.java
@@ -0,0 +1,34 @@
+package com.test.spring.di_7;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+public class File implements IWorker {
+
+	public ArrayList<String> list() {
+		
+		ArrayList<String> list = new ArrayList<String>();
+		
+		try {
+			
+			BufferedReader reader = new BufferedReader(new FileReader("data.db"));
+			
+			String temp = "";
+			
+			while ((temp = reader.readLine()) != null) {
+				list.add(temp);
+			}
+			
+			reader.close();
+			
+			return list;
+						
+		} catch (Exception e) {
+			System.out.println("Ex03_File.list : " + e.toString());
+		}
+		
+		return null;
+	}
+
+}
