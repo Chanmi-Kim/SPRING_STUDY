0eab25a197a24b93fd67427b510fbf3507d1d939
Chanmi Kim <chanmi_dev@naver.com>
2019-07-23 02:24:05 +0900

    Add File [SpringBegin > script.sql]
    1. sql 파일 추가
    
    - script.sql

---
+++ SpringBegin/script.sql
@@ -0,0 +1,13 @@
+-- D:\class\spring\SpringBegin\script.sql
+
+create table tblDI (
+    data varchar2(30) not null
+);
+
+insert into tblDI values ('홍길동');
+insert into tblDI values ('아무개');
+insert into tblDI values ('하하하');
+insert into tblDI values ('호호호');
+insert into tblDI values ('후후후');
+
+commit;
