ddd6f0fe5e554b95c2aaa55481ac676c6190c933
Chanmi-Kim <chanmi_dev@naver.com>
2019-07-24 20:16:47 +0900

    Add File [SpringWebTest > src > main > java > com.test.spring > InsaVOjava]
    1. java 파일 추가
    
    - InsaVO.java

---
+++ SpringWebTest/src/main/java/com/test/spring/InsaVO.java
@@ -0,0 +1,21 @@
+package com.test.spring;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class InsaVO {
+
+	private String num;
+	private String name;
+	private String buseo;
+	private String jikwi;
+	
+}
+
+
+
+
