5bdbb32c73f16c1aec71be7665e46db196d66c51
Chanmi Kim <chanmi_dev@naver.com>
2019-07-28 00:10:56 +0900

    Add File [SpringTilesTest > src > main > java > com.test.spring > MainController.java]
    1. java 파일 추가
    
    - MainController.java

---
+++ SpringTilesTest/src/main/java/com/test/spring/MainController.java
@@ -0,0 +1,65 @@
+package com.test.spring;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MainController {
+
+	@RequestMapping(value = "/member/info.action", method = { RequestMethod.GET })
+	public String info(HttpServletRequest req, HttpServletResponse resp, HttpSession session) {
+
+		//이전 ViewResolver : JSP 경로
+		//타일즈 ViewResolver : <definition>의 name값
+		return "member.info";//*.*
+	}
+	
+	
+	@RequestMapping(value = "/member/point.action", method = { RequestMethod.GET })
+	public String point(HttpServletRequest req, HttpServletResponse resp, HttpSession session) {
+
+		return "member.point";
+	}
+	
+	@RequestMapping(value = "/admin/data.action", method = { RequestMethod.GET })
+	public String data(HttpServletRequest req, HttpServletResponse resp, HttpSession session) {
+
+		return "admin.data";
+	}
+	
+	
+	@RequestMapping(value = "/admin/chart.action", method = { RequestMethod.GET })
+	public String chart(HttpServletRequest req, HttpServletResponse resp, HttpSession session) {
+
+		return "admin.chart";
+	}
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
