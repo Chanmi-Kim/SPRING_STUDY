e2b1c39ce2e200a9bc2ef6c3af8a505dcff43309
Chanmi-Kim <chanmi_dev@naver.com>
2019-07-29 16:36:15 +0900

    Add File [MyBatisDTO.java]
    1. java 파일 추가
    
    - MyBatisDTO.java

---
+++ SpringMyBatisTest/src/main/java/com/test/spring/MyBatisDTO.java
@@ -0,0 +1,59 @@
+package com.test.spring;
+
+public class MyBatisDTO {
+	
+	private String seq;
+	private String num;
+	private String txt;
+	
+	private String name;
+	private String buseo;
+	private String jikwi;
+	private String city;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBuseo() {
+		return buseo;
+	}
+	public void setBuseo(String buseo) {
+		this.buseo = buseo;
+	}
+	public String getJikwi() {
+		return jikwi;
+	}
+	public void setJikwi(String jikwi) {
+		this.jikwi = jikwi;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getSeq() {
+		return seq;
+	}
+	public void setSeq(String seq) {
+		this.seq = seq;
+	}
+	public String getNum() {
+		return num;
+	}
+	public void setNum(String num) {
+		this.num = num;
+	}
+	public String getTxt() {
+		return txt;
+	}
+	public void setTxt(String txt) {
+		this.txt = txt;
+	}
+	
+	
+
+}
