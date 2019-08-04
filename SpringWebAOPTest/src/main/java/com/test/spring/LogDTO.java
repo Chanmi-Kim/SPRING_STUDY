8985ace4fb545906fa2154ea2c4367dae1c6423e
Chanmi Kim <chanmi_dev@naver.com>
2019-08-04 21:34:57 +0900

    Add File [SpringWebAOPTest > LogDTO.java]
    1. java 파일 추가
    
    - LogDTO.java

---
+++ SpringWebAOPTest/src/main/java/com/test/spring/LogDTO.java
@@ -0,0 +1,22 @@
+package com.test.spring;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter @ToString
+public class LogDTO {
+	
+	private String seq;
+	private String url;
+	private String id;
+	private String regdate;
+
+}
+
+
+
+
+
+
+
